c13196ff661215cf0dfbce134572cd93d60a115c
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-12 17:37:29 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -19,10 +19,10 @@ func stringInSlice(a string, list []string) bool {
 
 func main() {
 	var buffer bytes.Buffer
-	fmt.Print("Welcome to Paranoid Files\nPlease select a mode (e, d): ")
+	fmt.Print("Welcome to Paranoid Files\nPlease select a mode:\n e: encrypt\n d: decrypt\n ")
 	mode := read()
 	fmt.Print("\n")
-	switch mode {
+	switch strings.ToLower(mode) {
 	case "e":
 		fmt.Print("Selected encrypt mode\n\n")
 		fmt.Print("Enter the name of the file to be encrypted (no extension): ")
@@ -39,8 +39,8 @@ func main() {
 			fmt.Println("File reading error:\n", err)
 			return
 		}
-		fmt.Print("Contents of file:", string(data), "\n\n\n")
-		fmt.Print("\tEncrypt? (Y/N): ")
+		fmt.Println("Contents of file:", string(data))
+		fmt.Print("\n\tEncrypt? (Y/N): ")
 		inp := read()
 		if strings.ToLower(inp) == "y" {
 			cont := string(data)
@@ -58,15 +58,71 @@ func main() {
 		wfilename := read()
 		_, err := ioutil.ReadFile(rfilename)
 		if err != nil {
-			fmt.Println("File reading error:\n", err)
+			fmt.Println("File reading error:\n\t", err)
 			return
 		}
-		fmt.Print("\tDecrypt? (Y/N): ")
+		fmt.Print("\n\tDecrypt? (Y/N): ")
 		inp := read()
 		if strings.ToLower(inp) == "y" {
 			err := ioutil.WriteFile(wfilename, []byte(decryption(rfilename)), 0644)
 			if err != nil {
 				log.Fatalf("error: %v", err)
+			}
+		}
+	case "b": // TODO
+		fmt.Print("Select mode:\n b: byteify\n u: unbyteify\n ")
+		m := read()
+		switch strings.ToLower(m) {
+		case "b":
+			fmt.Print("Type the name of the file to byteify (no extension): ")
+			name := read()
+			fmt.Print("\nType the file extension: ")
+			ext := read()
+			buffer.Reset()
+			buffer.WriteString(name)
+			buffer.WriteString(".")
+			buffer.WriteString(ext)
+			rfilename := buffer.String()
+			cont, err := ioutil.ReadFile(rfilename)
+			if err != nil {
+				fmt.Println("File reading error:\n\t", err)
+				return
+			}
+			fmt.Printf("\tContent of file is:\n\n%s\n", cont)
+			fmt.Print("\n\tByteify? (y/n)")
+			inp := read()
+			if strings.ToLower(inp) == "y" {
+				wfilename := name + ".bara"
+				bcont := []byte(cont)
+				err = ioutil.WriteFile(wfilename, bcont, 0644)
+				if err != nil {
+					log.Fatalf("error: %v", err)
+				}
+			}
+		case "u":
+			fmt.Print("Type the name of the file to unbyteify (no extension): ")
+			name := read()
+			fmt.Print("\nType the file extension: ")
+			ext := read()
+			buffer.Reset()
+			buffer.WriteString(name)
+			buffer.WriteString(".")
+			buffer.WriteString(ext)
+			rfilename := buffer.String()
+			cont, err := ioutil.ReadFile(rfilename)
+			if err != nil {
+				fmt.Println("File reading error:\n\t", err)
+				return
+			}
+			fmt.Print("\n\tUnbyteify? (y/n)")
+			inp := read()
+			if strings.ToLower(inp) == "y" {
+				fmt.Print("\nEnter the name of the file to be exported: ")
+				wfilename := read()
+				err = ioutil.WriteFile(wfilename, []byte(cont), 0644)
+				if err != nil {
+					log.Fatalf("error: %v", err)
+				}
 			}
 		}
 	case "exit":
