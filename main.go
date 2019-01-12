b7333200ef48387cd8a59f5d3cade5678ee98eb5
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-12 18:47:20 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -25,35 +25,26 @@ func main() {
 	switch strings.ToLower(mode) {
 	case "e":
 		fmt.Print("Selected encrypt mode\n\n")
-		fmt.Print("Enter the name of the file to be encrypted (no extension): ")
-		name := read()
-		fmt.Print("\nEnter the extension of the file: ")
-		ext := read()
-		buffer.WriteString(name)
-		buffer.WriteString(".")
-		buffer.WriteString(ext)
-		rfilename := buffer.String()
-		buffer.Reset()
+		fmt.Print("Enter the name of the file to be encrypted: ")
+		rfilename := read()
 		data, err := ioutil.ReadFile(rfilename)
 		if err != nil {
 			fmt.Println("File reading error:\n\t", err, "\n\nMake sure you're in the correct folder and that you spelled everything correctly.")
 			return
 		}
+		fmt.Print("Enter the name of the file to be exported: ")
+		wfilename := read()
 		fmt.Println("Contents of file:", string(data))
 		fmt.Print("\n\tEncrypt? (Y/N): ")
 		inp := read()
 		if strings.ToLower(inp) == "y" {
 			cont := string(data)
-			encryption(cont, name+".para")
+			encryption(cont, wfilename)
 		}
 	case "d":
 		fmt.Println("Selected decrypt mode\n")
-		fmt.Print("Enter the file to decrypt (no extension): ")
-		name := read()
-		buffer.WriteString(name)
-		buffer.WriteString(".para")
-		rfilename := buffer.String()
-		buffer.Reset()
+		fmt.Print("Enter the file to decrypt: ")
+		rfilename := read()
 		fmt.Print("Enter the name of the file you want exported: ")
 		wfilename := read()
 		_, err := ioutil.ReadFile(rfilename)
@@ -68,6 +59,7 @@ func main() {
 			if err != nil {
 				log.Fatalf("error: %v", err)
 			}
+			fmt.Printf("\nExported to file: %s", wfilename)
 		}
 	case "b": // TODO
 		fmt.Print("Select mode:\n b: byteify\n u: unbyteify\n ")
@@ -89,7 +81,7 @@ func main() {
 				return
 			}
 			fmt.Printf("\tContent of file is:\n\n%s\n", cont)
-			fmt.Print("\n\tByteify? (y/n)")
+			fmt.Print("\n\tByteify? (Y/N)")
 			inp := read()
 			if strings.ToLower(inp) == "y" {
 				wfilename := name + ".bara"
@@ -114,7 +106,7 @@ func main() {
 				fmt.Println("File reading error:\n\t", err, "\n\nMake sure you're in the correct folder and that you spelled everything correctly.")
 				return
 			}
-			fmt.Print("\n\tUnbyteify? (y/n)")
+			fmt.Print("\n\tUnbyteify? (Y/N)")
 			inp := read()
 			if strings.ToLower(inp) == "y" {
 				fmt.Print("\nEnter the name of the file to be exported: ")
