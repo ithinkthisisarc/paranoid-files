9457a02723152a96567621672e46fb50be1b13c5
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-14 11:37:10 -0500

    Made flags shorter for Maximum Speed

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	var svar string
-	flag.StringVar(&svar, "mode", "e", "Select a mode. Valid modes are:\n E: Encrypt\n D: Decrypt")
+	flag.StringVar(&svar, "m", "e", "Select a mode. Valid modes are:\n E: Encrypt\n D: Decrypt")
 
 	var svar2 string
-	flag.StringVar(&svar2, "target", "tester.txt", "Select the file to be encrypted")
+	flag.StringVar(&svar2, "t", "tester.txt", "Select the file to be encrypted")
 
 	var svar3 string
-	flag.StringVar(&svar3, "export", "tester.para", "Select the name of the exported file")
+	flag.StringVar(&svar3, "e", "tester.para", "Select the name of the exported file")
 	flag.Parse()
 	mode := svar
 	target := svar2
